6beff91c986b362cc3a0e4f38adf2919f7e022cd
Andrew Chappell <andrew@infinityimpossible.com>
2012-06-10 18:22:53 +0100

    Fixed build break.

--- pyflakes/test/test_other.py
+++ pyflakes/test/test_other.py
@@ -39,7 +39,6 @@ class Test(harness.Test):
         Test that shadowing a function definition in a class suite with another
         one raises a warning.
         """
-		foo.bar()
         self.flakes('''
         class A:
             def a(): pass
